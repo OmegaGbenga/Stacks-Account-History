2433beb330a13cb7eec52bd0c6ed6b6ea7f232bd
OmegaGbenga <Gbengadareowolabi@gmail.com>
2025-12-27 14:15:44 +0100

    feat: implement logic in stacks-connect.ts

--- web/lib/stacks-connect.ts
+++ web/lib/stacks-connect.ts
@@ -32,3 +32,6 @@ export function authenticate(options?: AuthenticateOptions) {
 }
 
 export function getUserData() {
+    if (userSession.isUserSignedIn()) {
+        return userSession.loadUserData();
+    }
