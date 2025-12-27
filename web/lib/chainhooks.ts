d48551f467c231c5d28ecbb228acf0346cd06358
OmegaGbenga <Gbengadareowolabi@gmail.com>
2025-12-27 14:15:51 +0100

    feat: export members from chainhooks.ts

--- web/lib/chainhooks.ts
+++ web/lib/chainhooks.ts
@@ -6,3 +6,8 @@ const API_KEY = process.env.NEXT_PUBLIC_HIRO_API_KEY;
  * Setup Chainhooks client
  */
 export const chainhooks = new ChainhooksClient({
+    apiKey: API_KEY,
+    network: 'mainnet' // or testnet
+});
+
+export async function registerHook(predicate: any) {
