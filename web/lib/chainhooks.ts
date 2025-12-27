6d4e5481a186188399b26cbd2d69a543fcdd1aac
OmegaGbenga <Gbengadareowolabi@gmail.com>
2025-12-27 14:15:51 +0100

    feat: export members from chainhooks.ts

--- web/lib/chainhooks.ts
+++ web/lib/chainhooks.ts
@@ -1,1 +1,8 @@
 import { ChainhooksClient } from '@hirosystems/chainhooks-client';
+
+const API_KEY = process.env.NEXT_PUBLIC_HIRO_API_KEY;
+
+/**
+ * Setup Chainhooks client
+ */
+export const chainhooks = new ChainhooksClient({
