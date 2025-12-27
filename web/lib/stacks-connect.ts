7630842fa234a1727f14039f80b1f61853a0b0d4
OmegaGbenga <Gbengadareowolabi@gmail.com>
2025-12-27 14:15:42 +0100

    feat: implement logic in stacks-connect.ts

--- web/lib/stacks-connect.ts
+++ web/lib/stacks-connect.ts
@@ -8,3 +8,6 @@ const appConfig = new AppConfig(['store_write', 'publish_data']);
 export const userSession = new UserSession({ appConfig });
 
 export interface AuthenticateOptions {
+  onFinish?: () => void;
+  onCancel?: () => void;
+}
