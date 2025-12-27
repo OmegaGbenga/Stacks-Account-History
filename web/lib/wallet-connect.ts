25bd316ec042afbe9bc90140ddf1309b9ffa135e
OmegaGbenga <Gbengadareowolabi@gmail.com>
2025-12-27 14:15:46 +0100

    feat: update wallet-connect.ts

--- web/lib/wallet-connect.ts
+++ web/lib/wallet-connect.ts
@@ -1,2 +1,10 @@
 import { Core } from '@walletconnect/core';
 import { Web3Wallet } from '@walletconnect/web3wallet';
+
+const PROJECT_ID = process.env.NEXT_PUBLIC_WC_PROJECT_ID || 'demo-id';
+
+/**
+ * Initialize WalletConnect Core
+ */
+const core = new Core({
+  projectId: PROJECT_ID
