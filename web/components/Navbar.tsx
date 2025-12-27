270956b6d097478d3ec8e1f60a17c4fc418cb3ff
OmegaGbenga <Gbengadareowolabi@gmail.com>
2025-12-27 14:16:17 +0100

    feat: export component from Navbar.tsx

--- web/components/Navbar.tsx
+++ web/components/Navbar.tsx
@@ -2,3 +2,6 @@ import React from 'react';
 import Link from 'next/link';
 import { Button } from './ui/Button';
 import { authenticate, getUserData } from '@/lib/stacks-connect';
+import { initWalletConnect } from '@/lib/wallet-connect';
+
+export const Navbar = () => {
